9a664e4f3d807f58dea6e05d9a5896cd70e5f6f5
Mykhailo Sheliahov <30924289+MykhailoSheliahov@users.noreply.github.com>
2023-11-01 21:39:58 +0200

    SQS & SNS, Async Microservices Communication (#4)
    Co-authored-by: Mykhailo Sheliahov <mykhailo_sheliahov@epam.com>

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
-  product: "https://jj432jy4wa.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://migontauw4.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://ginu783f1i.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://jj432jy4wa.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://luv9o6ph00.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://migontauw4.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
