1bc185851f7f48c20dfde1f6d71653d9257a9b75
Mykhailo Sheliahov <mykhailo_sheliahov@epam.com>
2023-11-17 17:48:30 +0200

    task-9: update cart API PATH

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://f7ay4fkya5.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://luv9o6ph00.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://migontauw4.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://f7ay4fkya5.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "http://mykhailo-sheliahov-cart-api-develop.eu-west-1.elasticbeanstalk.com/dev/api",
 };
 
 export default API_PATHS;
