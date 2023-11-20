70d8acb71e3b1a0da207025b42f48d5adeda97a2
Mykhailo Sheliahov <30924289+MykhailoSheliahov@users.noreply.github.com>
2023-11-20 18:01:45 +0200

    Update api path (#6)
    Co-authored-by: Mykhailo Sheliahov <mykhailo_sheliahov@epam.com>

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://migontauw4.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://f7ay4fkya5.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://luv9o6ph00.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://migontauw4.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://f7ay4fkya5.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
